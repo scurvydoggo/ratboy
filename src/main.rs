8e090a83a418ec6ea5d997b69409ff5c9cf57bef
scurvydoggo <andreas.nygard@gmail.com>
2025-09-03 22:45:13 +1000

    Render the scene in ratatui half pixels

--- src/main.rs
+++ src/main.rs
@@ -1,88 +1,49 @@
-use bevy::{
-    prelude::*,
-    render::{
-        camera::RenderTarget,
-        render_resource::{
-            Extent3d, TextureDescriptor, TextureDimension, TextureFormat, TextureUsages,
-        }
-    }
-};
+use std::time::Duration;
+use bevy::prelude::*;
+use bevy::app::ScheduleRunnerPlugin;
+use bevy::diagnostic::{DiagnosticsStore, FrameTimeDiagnosticsPlugin};
+use bevy::log::LogPlugin;
+use bevy::winit::WinitPlugin;
 use bevy_rapier2d::prelude::*;
-use crossterm::event::KeyCode;
-
-const TERMINAL_WIDTH: u32 = 80;
-const TERMINAL_HEIGHT: u32 = 10;
-
-#[derive(Component)]
-struct PixelBuffer {
-    handle: Handle<Image>
-}
+use bevy_ratatui::{RatatuiContext, RatatuiPlugins};
+use bevy_ratatui_camera::{RatatuiCamera, RatatuiCameraPlugin, RatatuiCameraStrategy, RatatuiCameraWidget};
+use ratatui::Frame;
+use ratatui::layout::{Constraint, Direction, Layout, Alignment};
+use ratatui::style::{Style, Stylize};
+use ratatui::widgets::{Block, Widget};
+use tui_logger::TuiLoggerWidget;
 
 fn main() {
     App::new()
         .add_plugins((
-            MinimalPlugins,
-            bevy_stdin::StdinPlugin,
-            bevy::asset::AssetPlugin::default(),
-            bevy::input::InputPlugin,
-            bevy::render::texture::ImagePlugin::default_nearest(), // Prevent blurry sprites
+            DefaultPlugins.build()
+                .disable::<WinitPlugin>()
+                .disable::<LogPlugin>(),
+            ScheduleRunnerPlugin::run_loop(Duration::from_secs_f64(1. / 60.)),
+        ))
+        .add_plugins((
+            // set up the Ratatui context and forward input events
+            RatatuiPlugins::default(),
+            // connect a bevy camera target to a ratatui widget
+            RatatuiCameraPlugin
         ))
         .add_plugins(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
         .add_systems(Startup, (setup_camera, setup_scene))
-        .add_systems(Update, print_terminal)
+        .add_systems(Update, print_scene)
         .insert_resource(ClearColor(Color::BLACK))
+        .init_resource::<Flags>()
         .run();
 }
 
-fn setup_camera(
-    mut commands: Commands,
-    mut images: ResMut<Assets<Image>>,
-) {
-    // Create an image to draw on
-    let image_handle = {
-        let size = Extent3d {
-            width: TERMINAL_WIDTH,
-            height: TERMINAL_HEIGHT,
-            ..default()
-        };
-        let mut image = Image {
-            texture_descriptor: TextureDescriptor {
-                label: None,
-                size,
-                dimension: TextureDimension::D2,
-                format: TextureFormat::Rgba8UnormSrgb,
-                mip_level_count: 1,
-                sample_count: 1,
-                usage: TextureUsages::COPY_DST
-                    | TextureUsages::COPY_SRC
-                    | TextureUsages::RENDER_ATTACHMENT,
-                view_formats: &[],
-            },
-            ..default()
-        };
-        image.resize(size);
-
-        images.add(image)
-    };
-
-    // Create a camera that draws to the image
+fn setup_camera(mut commands: Commands) {
     commands.spawn((
-        Camera2d::default(),
-        Camera {
-            target: RenderTarget::Image(image_handle.clone().into()),
-            ..default()
-        },
-        Transform::from_xyz(0.0, 0.0, 0.0)
+        RatatuiCamera::default(),
+        RatatuiCameraStrategy::luminance_braille(),
+        Camera2d,
     ));
-
-    // Track the image with an entity
-    commands.spawn(PixelBuffer { handle: image_handle.clone() });
 }
 
-fn setup_scene(
-    mut commands: Commands,
-    asset_server: Res<AssetServer>,
-) {
+fn setup_scene(mut commands: Commands, asset_server: Res<AssetServer>) {
     // Create the ground
     commands
         .spawn(Collider::cuboid(500.0, 50.0))
@@ -100,21 +61,68 @@ fn setup_scene(
         .insert(Transform::from_xyz(200.0, 800.0, 0.0));
 }
 
-fn print_terminal(
-    key: Res<ButtonInput<KeyCode>>,
-    pixel_buffer: Single<&PixelBuffer>,
-    images: Res<Assets<Image>>,
-) {
-    if key.just_pressed(KeyCode::Char(' ')) {
-        let image = images.get(pixel_buffer.handle.id()).expect("Image for pixel buffer not found");
-        for y in 0..TERMINAL_HEIGHT {
-            for x in 0..TERMINAL_WIDTH {
-                let pixel = image.get_color_at(x, y).expect("Pixel out of bounds");
-                let rgb = pixel.to_srgba();
-                let set = rgb.red != 0.0 || rgb.green != 0.0 || rgb.blue != 0.0;
-                print!("{}", if set { "x" } else { " " });
-            }
-            print!("<\r\n");
+fn print_scene(
+    mut ratatui: ResMut<RatatuiContext>,
+    mut camera_widget: Single<&mut RatatuiCameraWidget>,
+    flags: Res<Flags>,
+    diagnostics: Res<DiagnosticsStore>,
+) -> Result {
+    ratatui.draw(|frame| {
+        let area = debug_frame(frame, &flags, &diagnostics);
+
+        camera_widget.render(area, frame.buffer_mut());
+    })?;
+
+    Ok(())
+}
+
+#[allow(dead_code)]
+#[derive(Resource, Default)]
+pub struct Flags {
+    pub debug: bool,
+}
+
+#[allow(dead_code)]
+pub fn debug_frame(
+    frame: &mut Frame,
+    flags: &Flags,
+    diagnostics: &DiagnosticsStore,
+) -> ratatui::layout::Rect {
+    let mut block = Block::bordered()
+        .bg(ratatui::style::Color::Rgb(0, 0, 0))
+        .border_style(Style::default().bg(ratatui::style::Color::Black))
+        .title_bottom("[q for quit]")
+        .title_bottom("[d for debug]")
+        .title_alignment(Alignment::Center);
+
+    if flags.debug {
+        let layout = Layout::new(
+            Direction::Vertical,
+            [Constraint::Percentage(66), Constraint::Fill(1)],
+        )
+        .split(frame.area());
+
+        if let Some(value) = diagnostics
+            .get(&FrameTimeDiagnosticsPlugin::FPS)
+            .and_then(|fps| fps.smoothed())
+        {
+            block = block.title_top(format!("[fps: {value:.0}]"));
         }
+
+        let inner = block.inner(layout[0]);
+        frame.render_widget(block, layout[0]);
+        frame.render_widget(
+            TuiLoggerWidget::default()
+                .block(Block::bordered())
+                .style(Style::default().bg(ratatui::style::Color::Reset)),
+            layout[1],
+        );
+
+        inner
+    } else {
+        let inner = block.inner(frame.area());
+        frame.render_widget(block, frame.area());
+
+        inner
     }
 }
